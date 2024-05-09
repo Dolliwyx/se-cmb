c3ac5a67f0f241692ee020712a2cb2af32bdfcb4
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-05-09 17:07:07 +0800

    refactor: modify UI using tailwind

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->favicon(asset('images/favicon.png'))
             ->brandLogo(asset('images/plm-header.png'))
+            ->viteTheme('resources/css/filament/admin/theme.css')
             ->brandLogoHeight('4rem')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
