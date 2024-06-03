8bd7f62e7dee626fc941bd306e4c0b319320137c
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-06-03 22:41:15 +0800

    refactor: add plm logo and module name to brandLogo

---
+++ resources/views/brand-icon.blade.php
@@ -0,0 +1,6 @@
+<div class="flex gap-2">
+    <img src="{{ asset('/images/plm-icon.png') }}" alt="PLM Logo" class="h-14">
+    <div class="flex items-center justify-center">
+        <span class="text-lg font-bold line-clamp-1">{{ env('APP_NAME') }}</span>
+    </div>
+</div>
