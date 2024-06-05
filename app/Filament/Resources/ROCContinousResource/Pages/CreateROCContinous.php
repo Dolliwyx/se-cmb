2f28190d357308c37b23f4b90d2ad596e5c58ccc
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-06-05 15:41:33 +0800

    feat(CreateRecords): update properties based if OR exists

--- app/Filament/Resources/ROCContinousResource/Pages/CreateROCContinous.php
+++ app/Filament/Resources/ROCContinousResource/Pages/CreateROCContinous.php
@@ -3,10 +3,40 @@
 namespace App\Filament\Resources\ROCContinousResource\Pages;
 
 use App\Filament\Resources\ROCContinousResource;
-use Filament\Actions;
+use App\Models\ContinousReport;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateROCContinous extends CreateRecord
 {
     protected static string $resource = ROCContinousResource::class;
+
+    public bool $ORExists = false;
+
+    public function updated($propertyName)
+    {
+        if ($propertyName === 'data.or_number') {
+            $this->updateName();
+        }
+    }
+
+    protected function updateName()
+    {
+        $orNumber = $this->data['or_number'];
+        $existingReceipt = ContinousReport::where('or_number', $orNumber)->first();
+        if ($existingReceipt) {
+            $this->data['payor_name'] = $existingReceipt->payor_name;
+            $this->data['student_number'] = $existingReceipt->student_number;
+            $this->data['college'] = $existingReceipt->college;
+            $this->data['bank_name'] = $existingReceipt->bank_name;
+            $this->data['bank_number'] = $existingReceipt->bank_number;
+            $this->ORExists = true;
+        } else {
+            $this->data['payor_name'] = null;
+            $this->data['student_number'] = null;
+            $this->data['college'] = null;
+            $this->data['bank_name'] = null;
+            $this->data['bank_number'] = null;
+            $this->ORExists = false;
+        }
+    }
 }
--- app/Filament/Resources/ROCManualResource/Pages/CreateROCManual.php
+++ app/Filament/Resources/ROCManualResource/Pages/CreateROCManual.php
@@ -3,10 +3,40 @@
 namespace App\Filament\Resources\ROCManualResource\Pages;
 
 use App\Filament\Resources\ROCManualResource;
-use Filament\Actions;
+use App\Models\ManualReport;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateROCManual extends CreateRecord
 {
     protected static string $resource = ROCManualResource::class;
+
+    public bool $ORExists = false;
+
+    public function updated($propertyName)
+    {
+        if ($propertyName === 'data.or_number') {
+            $this->updateName();
+        }
+    }
+
+    protected function updateName()
+    {
+        $orNumber = $this->data['or_number'];
+        $existingReceipt = ManualReport::where('or_number', $orNumber)->first();
+        if ($existingReceipt) {
+            $this->data['payor_name'] = $existingReceipt->payor_name;
+            $this->data['student_number'] = $existingReceipt->student_number;
+            $this->data['college'] = $existingReceipt->college;
+            $this->data['bank_name'] = $existingReceipt->bank_name;
+            $this->data['bank_number'] = $existingReceipt->bank_number;
+            $this->ORExists = true;
+        } else {
+            $this->data['payor_name'] = null;
+            $this->data['student_number'] = null;
+            $this->data['college'] = null;
+            $this->data['bank_name'] = null;
+            $this->data['bank_number'] = null;
+            $this->ORExists = false;
+        }
+    }
 }
