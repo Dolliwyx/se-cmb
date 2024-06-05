8fef0d23962d669f7152ca50f324fcb4eeebcda0
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-06-05 15:32:52 +0800

    refactor(Models): create new version of continous and manual report models

---
+++ app/Models/ManualReport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+class ManualReport extends FinancialTransaction
+{
+    protected $table = 'financial_transactions';
+
+    protected $attributes = [
+        'transaction_type' => 1,
+    ];
+
+    protected $fillable = [
+        'or_number',
+        'payor_name',
+        'student_number',
+        'college',
+        'transaction_code',
+        'amount',
+        'remarks',
+        'bank_name',
+        'bank_number',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            $existingReceipt = static::where('or_number', $model->or_number)->first();
+            if ($existingReceipt) {
+                $model->payor_name = $existingReceipt->payor_name;
+                $model->student_number = $existingReceipt->student_number;
+                $model->college = $existingReceipt->college;
+                $model->bank_name = $existingReceipt->bank_name;
+                $model->bank_number = $existingReceipt->bank_number;
+            }
+        });
+    }
+}
