1e35ca2ef9746574d7f210d91fb0a352bbd6fdcc
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-06-06 09:51:19 +0800

    refactor: get total per month only instead of whole table

--- app/Filament/Widgets/MonthlyTotalTransaction.php
+++ app/Filament/Widgets/MonthlyTotalTransaction.php
@@ -13,8 +13,8 @@ class MonthlyTotalTransaction extends StatsOverviewWidget
     protected function getStats(): array
     {
         return [
-            Stat::make('Total collection for continuous receipt', "PHP " . DB::table('financial_transactions')->where('transaction_type', 0)->sum('amount')),
-            Stat::make('Total collection for manual receipt', "PHP " . DB::table('financial_transactions')->where('transaction_type', 1)->sum('amount')),
+            Stat::make('Total collection for continuous receipt this month', "PHP " . DB::table('financial_transactions')->where('transaction_type', 0)->whereMonth('created_at', date('m'))->sum('amount')),
+            Stat::make('Total collection for manual receipt this month', "PHP " . DB::table('financial_transactions')->where('transaction_type', 1)->whereMonth('created_at', date('m'))->sum('amount')),
             Stat::make('Total collection for this month', "PHP " . DB::table('financial_transactions')->whereMonth('created_at', date('m'))->sum('amount')),
         ];
     }
