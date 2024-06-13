dd0c8702b13888932d005d951cbe351c08bd9275
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-06-13 12:35:56 +0800

    feat: add daily statistic

--- app/Filament/Widgets/MonthlyTotalTransaction.php
+++ app/Filament/Widgets/MonthlyTotalTransaction.php
@@ -12,10 +12,20 @@ class MonthlyTotalTransaction extends StatsOverviewWidget
 
     protected function getStats(): array
     {
+        // get today's complete month
+        $month = date('F');
+        $year = date('Y');
+
+        // get complete month, day, and year
+        $date = date('F j, Y');
+
         return [
-            Stat::make('Total collection for continuous receipt this month', "PHP " . DB::table('financial_transactions')->where('transaction_type', 0)->whereMonth('created_at', date('m'))->sum('amount')),
-            Stat::make('Total collection for manual receipt this month', "PHP " . DB::table('financial_transactions')->where('transaction_type', 1)->whereMonth('created_at', date('m'))->sum('amount')),
-            Stat::make('Total collection for this month', "PHP " . DB::table('financial_transactions')->whereMonth('created_at', date('m'))->sum('amount')),
+            Stat::make("Total collection for continous receipt {$date}", "PHP " . DB::table('financial_transactions')->where('transaction_type', 0)->whereDate('created_at', date('y-m-d'))->sum('amount')),
+            Stat::make("Total collection for manual receipt {$date}", "PHP " . DB::table('financial_transactions')->where('transaction_type', 1)->whereDate('created_at', date('y-m-d'))->sum('amount')),
+            Stat::make("Total collection for {$date}", "PHP " . DB::table('financial_transactions')->whereDate('created_at', date('y-m-d'))->sum('amount')),
+            Stat::make("Total collection for continuous receipt {$month} {$year}", "PHP " . DB::table('financial_transactions')->where('transaction_type', 0)->whereMonth('created_at', date('m'))->sum('amount')),
+            Stat::make("Total collection for manual receipt {$month} {$year}", "PHP " . DB::table('financial_transactions')->where('transaction_type', 1)->whereMonth('created_at', date('m'))->sum('amount')),
+            Stat::make("Total collection for {$month} {$year}", "PHP " . DB::table('financial_transactions')->whereMonth('created_at', date('m'))->sum('amount')),
         ];
     }
 }
